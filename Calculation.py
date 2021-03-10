30b9ccf8a3cce5594393a85945b53e2e8a11ba0d
FilipMirceski <mirceski.filip95@gmail.com>
2021-03-10 13:21:58 +0100

    Uploda new

--- Calculation.py
+++ Calculation.py
@@ -1,53 +1,29 @@
 import bpy
 import math
 
+wlk_collection = 'Novi Test'
 
-a = [(1,2),(3,5),(5,6),(7,8)]
-b = [1,3,3,45,4,5,3,345,5,4,2,0,4,3,3,2,1,2,43,5]
 
 def main(context):
-    print ("uspeo si svaka cast")
-    print (a[1][1])
-    print ("test niz")
-    #x = b.index(3)
-    indexes = [i for i,x in enumerate(b) if x <= 0]
-    print (indexes)
+    print("")
+    print("")
+    print("################################## WLK: INIT")
+    print("")
 
-x1 = 10
-y1 = 0
-x2 = 25
-y2 =0
-
-def pozicija(context):
-    #lista u kom smestamo lokaciju
-    z = []
-    x = []
-    #uzimamo duzinu akcije
-    frameRange = int(bpy.data.actions['CubeDance'].frame_range[1])
-    cube=bpy.data.objects['Cube']
-    print (frameRange)
-    #iteracija kroz frejmove i uzimanje lokacije
-    for frame in range(0,frameRange):
-        bpy.context.scene.frame_set(frame)
-        #ver = cube.location.z
-        #hor = cube.location.x
-        #sid = 
-        z.append(lokacija_z)
-        x.append(lokacija_x)
-
-    print ("z je")
-    print (z)
-    print ("x je")
-    print (x)
+def new_collection(name):
     
+    collection = bpy.data.collections.get( name )
+    if collection == None:
+        collection = bpy.data.collections.new( name )
+        
+    if bpy.context.scene.collection.children.get( collection.name ) == None:
+        bpy.context.scene.collection.children.link( collection )
+        
+    print('Create new collection', name)
+   
+    return collection
 
-#funkcija za izracunavanje duzinu koraka
-def distance(x1,y1,x2,y2):
-    dist = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
-    #dist = math.hypot(x2 - x1, y2 - y1)
-    print (dist)
-    return dist
-
+ #new_collection(wlk_collection)
 
 
 class SimpleOperator(bpy.types.Operator):
@@ -61,14 +37,15 @@ class SimpleOperator(bpy.types.Operator):
 
     def execute(self, context):
         main(context)
-        distance(x1,y1,x2,y2)
-        pozicija(context)
+        new_collection(wlk_collection)
+        
+        
         return {'FINISHED'}
 
 
 def register():
     bpy.utils.register_class(SimpleOperator)
-
+    
 
 def unregister():
     bpy.utils.unregister_class(SimpleOperator)
